1f28adb82b5250f93394420a0d510b3a4f57cf15
Vladyslav Manoilo <vladyslav.manoilo@sobis.com>
2026-01-26 16:55:10 +0100

    Apendix A

--- pytorch.py
+++ pytorch.py
@@ -28,12 +28,15 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256,stride=128, shuffle=T
 
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
-dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False)
 data_iter = iter(dataloader)
-first_batch = next(data_iter)
-print(first_batch)
-second_batch = next(data_iter)
-print(second_batch)
+inputs, targets = next(data_iter)
+print("Inputs:\n", inputs)
+print("\nTargets:\n", targets)
+# first_batch = next(data_iter)
+# print(first_batch)
+# second_batch = next(data_iter)
+# print(second_batch)
 # input_ids = torch.tensor([2, 3, 5, 1])
 # vocab_size = 6
 # output_dim = 3
