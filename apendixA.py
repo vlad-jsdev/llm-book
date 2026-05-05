4f30c39587f9fc95b0a708d3babd0829bd8916c6
Vladyslav Manoilo <vladyslav.manoilo@sobis.com>
2026-05-05 17:12:50 +0200

    271

--- apendixA.py
+++ apendixA.py
@@ -1,33 +1,55 @@
 import torch
 
-class NeuralNetwork(torch.nn.Module):
-    def __init__(self, num_inputs, num_outputs):
-        super().__init__()
-
-        self.layers = torch.nn.Sequential(
-			torch.nn.Linear(num_inputs, 30),
-			torch.nn.ReLU(),
-
-			torch.nn.Linear(30, 20),
-			torch.nn.ReLU(),
-
-			torch.nn.Linear(20, num_outputs),
-		)
-
-    def forward(self, x):
-        logits = self.layers(x)
-        return logits
-
+# class NeuralNetwork(torch.nn.Module):
+#     def __init__(self, num_inputs, num_outputs):
+#         super().__init__()
+#
+#         self.layers = torch.nn.Sequential(
+# 			torch.nn.Linear(num_inputs, 30),
+# 			torch.nn.ReLU(),
+#
+# 			torch.nn.Linear(30, 20),
+# 			torch.nn.ReLU(),
+#
+# 			torch.nn.Linear(20, num_outputs),
+# 		)
+#
+#     def forward(self, x):
+#         logits = self.layers(x)
+#         return logits
+#
+# # model = NeuralNetwork(50, 3)
+# # num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+#
+# torch.manual_seed(123)
 # model = NeuralNetwork(50, 3)
-# num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-
-torch.manual_seed(123)
-model = NeuralNetwork(50, 3)
-X = torch.randn(1, 50)
-# output = model(X)
-# print(output)
-with torch.no_grad():
-	out = torch.softmax(model(X), dim=-1)
-print(out)
+# X = torch.randn(1, 50)
+# # output = model(X)
+# # print(output)
+# with torch.no_grad():
+# 	out = torch.softmax(model(X), dim=-1)
+# print(out)
 # print(f"Number of trainable parameters: {num_params}")
 # print(model)
+#
+X_train = torch.tensor([
+	[-1.2, 3.1],
+	[-0.9, 2.9],
+	[-0.5, 2.6],
+	[2.3, -1.1],
+	[2.7, -1.5]
+])
+y_train = torch.tensor([0, 0, 0, 1, 1])
+
+X_test = torch.tensor([
+	[-0.8, 2.8],
+	[2.6, -1.6]
+])
+
+y_test = torch.tensor([0, 1])
+
+
+from torch.utils.data import Dataset
+
+class ToyDataset(Dataset):
+    def __init__(self, X, y):
