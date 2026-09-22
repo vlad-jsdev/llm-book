8dbb26817b48a57106c379fb9009ad9e55777df3
Vladyslav Manoilo <vladyslav.manoilo@sobis.com>
2026-09-22 17:09:03 +0200

    page 90

--- llm-sample.py
+++ llm-sample.py
@@ -85,18 +85,18 @@ import torch
 #
 #
 
-inputs = torch.tensor(
-	[[0.43, 0.15, 0.89], # Your (x^1)
-	[0.55, 0.87, 0.66], # journey (x^2)
-	[0.57, 0.85, 0.64], # starts (x^3)
-	[0.22, 0.58, 0.33], # with (x^4)
-	[0.77, 0.25, 0.10], # one (x^5)
-	[0.05, 0.80, 0.55]] # step (x^6#
-)
-
-query = inputs[1]
-attn_scores_2 = torch.empty(inputs.shape[0])
-attn_weights_2 = torch.softmax(attn_scores_2, dim=0) 
+# inputs = torch.tensor(
+# 	[[0.43, 0.15, 0.89], # Your (x^1)
+# 	[0.55, 0.87, 0.66], # journey (x^2)
+# 	[0.57, 0.85, 0.64], # starts (x^3)
+# 	[0.22, 0.58, 0.33], # with (x^4)
+# 	[0.77, 0.25, 0.10], # one (x^5)
+# 	[0.05, 0.80, 0.55]] # step (x^6#
+# )
+#
+# query = inputs[1]
+# attn_scores_2 = torch.empty(inputs.shape[0])
+# attn_weights_2 = torch.softmax(attn_scores_2, dim=0) 
 # print("Attention weights:", attn_weights_2) 
 # print("Sum:", attn_weights_2.sum())
 # for i, x_i in enumerate(inputs):
@@ -110,10 +110,25 @@ attn_weights_2 = torch.softmax(attn_scores_2, dim=0)
 # 	for j, x_j in enumerate(inputs):
 
 # 		attn_scores[i, j] = torch.dot(x_i, x_j) 
-attn_scores = inputs @ inputs.T
-# print(attn_scores)
+# attn_scores = inputs @ inputs.T
+# # print(attn_scores)
+#
+# attn_weights = torch.softmax(attn_scores, dim=-1) 
+# # print(attn_weights)
+# all_context_vecs = attn_weights @ inputs
+# print(all_context_vecs)
 
-attn_weights = torch.softmax(attn_scores, dim=-1) 
-# print(attn_weights)
-all_context_vecs = attn_weights @ inputs
-print(all_context_vecs)
+
+
+x_2 = inputs[1]
+d_in = inputs.shape[1]
+d_out = 2
+
+torch.manual_seed(123)
+W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False) 
+W_key = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False) 
+W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+query_2 = x_2 @ W_query
+key_2 = x_2 @ W_key
+value_2 = x_2 @ W_value
+print(query_2)
